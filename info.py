4c901a94ad081842eb7ee762490d3db58e18dd24
darkdevil9234 <130627650+darkdevil9234@users.noreply.github.com>
2023-05-20 09:39:13 +0530

    Update info.py

--- info.py
+++ info.py
@@ -1,8 +1,8 @@
-API_ID       = 
+API_ID       = ""
 API_HASH     = ""
 BOT_TOKEN    = ""
 SESSION      = ""
 DATABASE_URI = ""
-LOG_CHANNEL  = -1001884373226
-ADMIN        = 1350642629
-CHANNEL      = "@GreyMatter_Bots"
+LOG_CHANNEL  = -1001801471389
+ADMIN        = 5007198306
+CHANNEL      = "@cinemaa_boxoffice"
